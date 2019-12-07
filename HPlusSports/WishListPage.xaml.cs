578f24de202eaa9e51fa0db55abefa9a0d3f3217
EthanChappel <EthanChappel@users.noreply.github.com>
2019-12-07 13:54:35 -0600

    Create new page for wish list

---
+++ HPlusSports/WishListPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using HPlusSports.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HPlusSports
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WishListPage : ContentPage
+    {
+        public List<Product> Products { get; set; }
+
+        public WishListPage()
+        {
+            InitializeComponent();
+            Products = ProductService.GetWishListProducts();
+            BindingContext = Products;
+        }
+
+        public void Item_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            Product product = e.CurrentSelection.First() as Product;
+            Navigation.PushAsync(new ProductDetail(product));
+        }
+    }
+}
