db5b197e715bf0329886b0e52134201a2f53b768
EthanChappel <EthanChappel@users.noreply.github.com>
2019-12-08 03:58:38 -0600

    Add order history page

---
+++ HPlusSports/OrderHistory.xaml.cs
@@ -0,0 +1,28 @@
+﻿using HPlusSports.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HPlusSports
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrderHistory : ContentPage
+    {
+        public ObservableCollection<Tuple<DateTime, int>> Items { get; set; }
+
+        public OrderHistory(Product p)
+        {
+            InitializeComponent();
+
+            Items = new ObservableCollection<Tuple<DateTime, int>>(ProductService.OrderHistory[p.Id]);
+
+            MyListView.ItemsSource = Items;
+        }
+    }
+}
